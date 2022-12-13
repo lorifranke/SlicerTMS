dae015ec121c337774f6188bed34d33c0dd5c88e
Loraine <38534852+lorifranke@users.noreply.github.com>
2022-12-13 14:00:00 +0100

    fix cnn bug

--- client/SlicerTMS/Loader.py
+++ client/SlicerTMS/Loader.py
@@ -4,6 +4,7 @@ from slicer.ScriptedLoadableModule import *
 # from slicerserver import Server
 # import nibabel as nib
 import numpy as np
+# from subprocess import check_output
 import subprocess
 
 import Mapper as M
@@ -11,6 +12,7 @@ import SlicerTMS as S
 
 
 class Loader:
+
     def __init__(self, data_directory):
 
         self.data_directory = data_directory
@@ -19,7 +21,7 @@ class Loader:
         self._coil_file = 'coil.stl'
         self._coil_scale = 3
         self._skin_file = 'skin.stl'
-        # self._magnorm_file = 'magnorm.nii.gz'
+        self._magnorm_file = 'magnorm.nii.gz'
         self._magfield_file = 'magfield.nii.gz'
         self._conductivity_file = 'conductivity.nii.gz'
 
@@ -32,7 +34,7 @@ class Loader:
         self.conductivityNode = None
         self.magfieldGTNode = None
         self.magfieldNode = None
-        # self.magnormNode = None
+        self.magnormNode = None
         self.efieldNode = None
         self.enormNode = None
         self.coilDefaultMatrix = vtk.vtkMatrix4x4()
@@ -42,36 +44,25 @@ class Loader:
         self.showMag = False #switch between magnetic and electric field for visualization
 
     def callMapper(self, param1=None, param2=None):
+        '''
+        '''
         M.Mapper.map(self)
 
 
     def newImage(self, caller, event):
-        print('New pyigtl image received from CNN')
+        print('new pyigtl image received')
         M.Mapper.modifyIncomingImage(self)
-
-
-    # def loadBrain(self):
-    #     self.modelNode.SetDisplayVisibility(1)
-
-    @staticmethod
-    def loadFibers(self):
-        loader.fiberNode.SetDisplayVisibility(1)
 
 
     @staticmethod
     def loadExample(self, example_path):
-        # example_path is the flag which the user has selected
+
         print('Your example: ' + example_path)
         data_directory = os.path.join(os.path.dirname(slicer.modules.slicertms.path), '../', example_path)
 
         loader = Loader(data_directory)
 
-        # slicer.mrmlScene.Clear() # had to uncomment this otherwise it would have removed the OpenIGTLink connectors
-
-        # executing commands from inside SLicer: this should start the server for the CNN (need to check if another process is running, if yes terminate)
-        # command_result = subprocess.Popen(["/usr/bin/python3", "-c", "../../server/server.py", "../../data/Example1/"], shell=True, env=slicer.util.startupEnvironment())
-        # command_result.wait()
-        # poll = command_result.poll()
+        slicer.mrmlScene.Clear()
 
         #
         # 1. Brain:
@@ -97,7 +88,6 @@ class Loader:
         loader.skinNode = slicer.modules.models.logic().AddModel(skin, slicer.vtkMRMLStorageNode.CoordinateSystemRAS)
         skinDisplayNode = loader.skinNode.GetDisplayNode()
         skinDisplayNode.SetOpacity(0.3)
-
 
 
         #
@@ -135,10 +125,10 @@ class Loader:
         # 5. Other stuff
         #
 
-        # # load magnorm (used for tesing and visualization, not useful for predicting E-field)
-        # loader.magnormNode = slicer.util.loadVolume( os.path.join( loader.data_directory, loader._magnorm_file ) )
-        # loader.magnormNode.SetName('MagNorm')
-        # loader.magnormNode.GetIJKToRASMatrix(loader.coilDefaultMatrix)
+        # load magnorm (used for tesing and visualization, not useful for predicting E-field)
+        loader.magnormNode = slicer.util.loadVolume( os.path.join( loader.data_directory, loader._magnorm_file ) )
+        loader.magnormNode.SetName('MagNorm')
+        loader.magnormNode.GetIJKToRASMatrix(loader.coilDefaultMatrix)
 
 
         # load magvector as a GridTransformNode 
@@ -170,7 +160,7 @@ class Loader:
         loader.IGTLNode = slicer.vtkMRMLIGTLConnectorNode()
         slicer.mrmlScene.AddNode(loader.IGTLNode)
         # node should be visible in OpenIGTLinkIF module under connectors
-        loader.IGTLNode.SetName('CNN_Connector')
+        loader.IGTLNode.SetName('Connector1')
         # add command line stuff here
         loader.IGTLNode.SetTypeClient('localhost', 18944)
         # this will activate the the status of the connection:
@@ -184,7 +174,6 @@ class Loader:
         loader.pyigtlNode = slicer.mrmlScene.AddNewNodeByClass('vtkMRMLVectorVolumeNode', 'pyigtl_data')
         observationTag = loader.pyigtlNode.AddObserver(slicer.vtkMRMLVectorVolumeNode.ImageDataModifiedEvent, loader.newImage)
 
-         
         # # call one time
         loader.callMapper()
 
@@ -194,4 +183,3 @@ class Loader:
         #slicer.mrmlScene.AddObserver(slicer.vtkMRMLScene.NodeAddedEvent, loader.onNodeRcvd)
 
         return loader
-
