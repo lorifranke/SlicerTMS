96bf311a314779f2924333d214d556efcaa56425
Loraine <38534852+lorifranke@users.noreply.github.com>
2023-01-20 13:15:00 +0100

    found way to send polydata via websocket, convert to binary data, json did not work

--- client/SlicerTMS/requesthandlers/handle_websocket.py
+++ client/SlicerTMS/requesthandlers/handle_websocket.py
@@ -2,8 +2,16 @@ import json
 import sys
 import time
 import numpy
+import json
+import numpy as np
+from vtk.util.numpy_support import vtk_to_numpy
+from vtk.util import numpy_support
 from __main__ import vtk, qt, ctk, slicer
 from slicer.ScriptedLoadableModule import *
+
+import zlib
+import base64
+
 
 try:
     import urlparse
@@ -35,13 +43,17 @@ class SlicerWebSocketHandler(WebSocketHandler):
     def on_close(self) -> None:
         super().on_close()
 
-    def on_message(self, message: Union[str, bytes]) -> Optional[Awaitable[None]]:
+    def on_message(self, message):
         # print("Coil Position Matrix:")
         # self.logMessage(message)
-        self.on_pose(message)
-        if message == 'get_coil_position':
-            # you can access slicer code here
-            self.write_message('1.2323 3.34324 5.34324')
+        # self.on_pose(message)
+        if message == "get_node":
+            self.on_get_node(message)
+        else:
+            self.on_pose(message)
+        # if message == 'get_coil_position':
+        #     # you can access slicer code here
+        #     self.write_message('1.2323 3.34324 5.34324')
 
     def on_pose(self, message):
         p = urlparse.urlparse(message)
@@ -107,9 +119,98 @@ class SlicerWebSocketHandler(WebSocketHandler):
                     m.SetElement(row, column, m3[row][column])
 
         self.tracker.SetMatrixTransformToParent(m)
-        print('sending stuff!!')
         # this method needs to be called to update the efield on the brain:
         # slicer.modules.SlicerTMSWidget.onHandlesModified()
         # slicer.modules.slicertms.onHandlesModified()
+
+
+    def on_get_node(self, message: Union[str, bytes]) -> Optional[Awaitable[None]]:
+        print('CALLED GETNODE!')
+        self.gm = slicer.util.getNode('gm')
+        polyData = self.gm.GetPolyData()
+
+        # Create a vtkPolyDataWriter object
+        writer = vtk.vtkPolyDataWriter()
+
+        # Set the input data to the polydata
+        writer.SetInputData(polyData)
+
+        # Write the binary data directly to a string
+        writer.WriteToOutputStringOn()
+
+        # Write the binary data
+        writer.Write()
+
+        # Get the binary data as a string
+        binary_data = writer.GetOutputString()
+        
+        # Convert binary_data to bytes
+        binary_data = bytes(binary_data, 'utf-8')
+
+        self.write_message(binary_data)
+
+        # # Compress the binary data using zlib
+        # compressed_data = zlib.compress(binary_data)
+
+        # # Encode the compressed data to a string
+        # encoded_data = base64.b64encode(compressed_data)
+
+        # # Send the encoded data through the websocket
+        # self.write_message(encoded_data)
+
+
+
+        ### APPROACH 1:
+        # # Create a vtkJSONWriter object
+        # writer = vtk.vtkJSONDataSetWriter()
+        # # Set the input data to the polydata
+        # writer.SetInputData(polyData)
+        # # Update the writer
+        # writer.Update() # this makes slicer crash
+        # # Get the output data
+        # output = writer.GetOutput()
+        # # Convert the vtkDataObject to a python dictionary
+        # data = json.loads(output.toJSON())
+        # # Print the JSON data
+        # print(json.dumps(data))
+
+
+        ######## APPROACH 2:
+        # # Convert vtkPolyData to vtkTable
+        # polyDataToTable = vtk.vtkPolyDataToTable()
+        # polyDataToTable.SetInputData(polyData)
+        # polyDataToTable.Update()
+
+        # table = polyDataToTable.GetOutput()
+
+        # # Write vtkTable to JSON file with compression
+        # jsonWriter = vtk.vtkJSONDataSetWriter()
+        # jsonWriter.SetInputData(table)
+        # jsonWriter.SetCompression(True)
+        # jsonWriter.WriteToOutputStringOn()
+        # jsonWriter.Update()
+        # json_string = jsonWriter.GetOutputString()
+        # json_data = json.loads(json_string)
+        # print(json_data)
+        # self.write_message(json_data)
+
+        #### APPROACH 3 with downsampling:
+
+        # self.gm = slicer.util.getNode('gm')
+        # mesh_data = self.gm.GetPolyData() # Get the mesh data from the mesh node
+        # # downsample the mesh before sending it
+        # decimate = vtk.vtkDecimatePro()
+        # decimate.SetInputData(mesh_data)
+        # decimate.SetTargetReduction(0.9) # set the target reduction factor to 0.9
+        # decimate.Update()
+        # decimated_mesh = decimate.GetOutput()
+        # points = decimated_mesh.GetPoints().GetData()
+        # points_np = vtk_to_numpy(points)
+        # cells = decimated_mesh.GetPolys().GetData()
+        # cells_np = vtk_to_numpy(cells)
+        # json_mesh = json.dumps({"points":points_np.tolist(),"cells":cells_np.tolist()})
+        # print(json_mesh)
+        # self.write_message(json_mesh)
+
 
 
